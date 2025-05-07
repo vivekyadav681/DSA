90826e9e3f2bef7d520adc8df5a2575fe1ac61d3
Vivek Yadav <vivekyadav00681@gmail.com>
2025-05-07 16:15:02 +0530

    init commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello duniya");
+        List<Integer> list = List.of(2, 3, 4, 5, 6, 7);
+        System.out.println(list.get(4));
+
+    }
+}
