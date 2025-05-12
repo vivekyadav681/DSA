acfef146bdf5ae6990c0278dd5d20d18fd3fab22
Vivek Yadav <vivekyadav00681@gmail.com>
2025-05-12 17:01:56 +0530

    init commit

---
+++ src/LinkedList.java
@@ -0,0 +1,15 @@
+public class LinkedList {
+    private Node first;
+    private Node last;
+
+    public void addFirst(int value) {
+
+    }
+
+    //addfirst
+    //addLast
+    //deleteFirst
+    //deleteLast
+    //contains
+    //indexOf
+}
